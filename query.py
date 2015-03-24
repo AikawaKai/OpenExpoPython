357818c7320a6d88175c2187bd2c5738222d139e
AikawaKai <marco.odore@studenti.unimi.it>
2015-03-24 01:37:02 +0100

    modificata query per includere più informazioni. Eliminata per il momento funzione query subappalti

--- query.py
+++ query.py
@@ -7,11 +7,13 @@ s =  fileobj.read()
 d = json.loads(s)
 result = d['result']
 records = result['records']
-resultquerystring = ''
+resultquerystring = '\nImporto appalti '+azienda+'\n\n'
 for prova in records:
 	if ((prova['Aggiudicatario']).lower()).find(azienda)!=-1:
-		resultquerystring =resultquerystring+ 'Cantiere: '+prova['Cantiere']+'\nAzienda: '+prova['Aggiudicatario'] + '\nImporto complessivo appalto: '+prova['Importo complessivo appalto']+'\n\n'
+		    resultquerystring = 'n'+resultquerystring +'Importo totale attuazione sicurezza: '+prova['Importo totale attuazione sicurezza']+'\n'+'Importo totale somme a disposizione: '+prova['Importo totale somme a disposizione']+'\n'+'Cantiere: '+prova['Cantiere']+'\n'+'Stato: '+prova['Stato']+'\n'+'Aggiudicatario: '+prova['Aggiudicatario']+'\n'+'Importo componente lavori: '+prova['Importo componente lavori']+'\n'+'Oggetto del lotto: '+prova['Oggetto del lotto']+'\n'+'Importo progettazione: '+prova['Importo progettazione']+'\n'+'Numero gara: '+prova['Numero gara']+'\n'+'Importo componente servizi: '+prova['Importo componente servizi']+'\n'+'Somme non assoggettate a ribasso: '+prova['Somme non assoggettate a ribasso']+'\n'+'Importo componente forniture: '+prova['Importo componente forniture']+'\n'+'Importo complessivo intervento: '+prova['Importo complessivo intervento']+'\n'+'Importo complessivo appalto: '+prova['Importo complessivo appalto']+'\n'+'Oggetto della gara: '+prova['Oggetto della gara']+'\n'+'ID lotto: '+prova['ID lotto']+'\n\n\n'
 
 print resultquerystring
-with open(azienda+".txt", "w") as text_file:
+with open('Appalti '+azienda+".txt", "w") as text_file:
     text_file.write(resultquerystring)
+
+#to do: query per subappalti 
